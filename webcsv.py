d05282e81c5d150a55be321e58a106dc4e672539
Julian <137456279+julianolschwang@users.noreply.github.com>
2024-02-26 19:09:07 -0800

    Update webcsv.py

--- webcsv.py
+++ webcsv.py
@@ -52,7 +52,7 @@ def read():
         reader = csv.DictReader(csvfile)
         for row in reader:
             data.append(dict(row))
-     return render_template_string('''
+    return render_template_string('''
         <html>
             <head>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
